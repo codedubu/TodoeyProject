177e1e9c0b250a0c2646a66ae7dbefe0e9a94097
codedubu <mccaine.river@gmail.com>
2020-12-07 15:13:48 -0800

    Adds CategoryViewController into controllers, updates storyboard to match, and sets basic outline of CVC.

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by River McCaine on 12/7/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - Data Manipulation Methods
+    
+    //MARK: - Add New Categories
+    
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+    }
+    //MARK: - Tableview Delegate Methods
+
+
+    
+}
