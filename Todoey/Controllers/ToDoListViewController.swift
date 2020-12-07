217e41fc0e18f92107d7915a767c728252a9af47
codedubu <mccaine.river@gmail.com>
2020-12-07 14:26:51 -0800

    Add search bar functionality with search parameters and update storyboard to include the search bar. (Missing ability to return to original list after searching)

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -17,7 +17,7 @@ class ToDoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+//        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
         loadItems()
     }
@@ -42,7 +42,12 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        //        context.delete(itemArray[indexPath.row])
+        //        itemArray.remove(at: indexPath.row)
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        
         self.saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -79,7 +84,6 @@ class ToDoListViewController: UITableViewController {
     //MARK: - Model Manipulation Methods
     func saveItems() {
         
-        
         do {
             try context.save()
         } catch {
@@ -89,19 +93,40 @@ class ToDoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+
         do {
             itemArray =  try context.fetch(request)
         } catch {
             print ("Error fetching data from context \(error)")
         }
+        
+        tableView.reloadData()
     }
     
     
-    
-    
-    
+}
+
+//MARK: - Search Bar Methods
+
+extension ToDoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+        
+//        do {
+//            itemArray =  try context.fetch(request)
+//        } catch {
+//            print ("Error fetching data from context \(error)")
+//        }
+        
+        
+    }
 }
 
 
